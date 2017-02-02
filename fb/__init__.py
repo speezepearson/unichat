1bcff0a01123a4ca7c0dd4310dd84a01f5310d8f
Spencer Pearson <suspense@cs.washington.edu>
2017-02-02 14:32:37 -0800

    create Facebook Client class with single subprocess

--- fb/__init__.py
+++ fb/__init__.py
@@ -11,27 +11,42 @@ HERE = os.path.dirname(__file__)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def iter_messages(get_username_from_id, get_channelname_from_id):
-  p = subprocess.Popen(['node', os.path.join(HERE, 'fetch.js')], stdout=subprocess.PIPE)
-  for fb_threadid, fb_speakerid, content in csv.reader(io.TextIOWrapper(p.stdout)):
-    logger.debug('got message: {}'.format((fb_threadid, fb_speakerid, content)))
-    yield Message(
+class Client:
+  def __init__(self):
+    self.process = subprocess.Popen(['node', os.path.join(HERE, 'client.js')], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    # self.process = subprocess.Popen(['cut','-d',',','-f','2-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    self._sending_buffer = io.StringIO()
+    self._sending_writer = csv.DictWriter(self._sending_buffer, fieldnames=Message._fields)
+    self._sending_writer.writeheader()
+    self._receiving_reader = csv.reader(io.TextIOWrapper(self.process.stdout))
+
+  def send(self, message):
+    logger.info('sending {} to Facebook'.format(message))
+    self._sending_writer.writerow(message._replace(thread=message.thread)._asdict())
+    self.process.stdin.write(self._sending_buffer.getvalue().encode() + b'\n')
+    logger.debug('wrote {!r} to client subprocess'.format(self._sending_buffer.getvalue().encode() + b'\n'))
+    self.process.stdin.flush()
+    self._sending_buffer.seek(0)
+    self._sending_buffer.truncate()
+
+  def read(self):
+    r = next(self._receiving_reader)
+    logger.info('received {} from Facebook'.format(r))
+    fb_threadid, fb_speakerid, content = r
+    return Message(
       time=dt.datetime.now(),
-      thread=get_channelname_from_id(fb_threadid),
-      speaker=get_username_from_id(fb_speakerid),
+      thread=fb_threadid,
+      speaker=fb_speakerid,
       content=content)
 
-def send_messages(messages, get_channelid_from_name):
-  p = subprocess.Popen(['node', os.path.join(HERE, 'send.js')], stdin=subprocess.PIPE)
-  sio = io.StringIO()
-  writer = csv.DictWriter(sio, fieldnames=Message._fields)
-  writer.writeheader()
-  for message in messages:
-    print('writing:', message)
-    writer.writerow(message._replace(thread=get_channelid_from_name(message.thread))._asdict())
-    p.stdin.write(sio.getvalue().encode() + b'\n')
-    p.stdin.flush()
-    sio.seek(0)
-    sio.truncate()
-  p.stdin.close()
-  p.wait()
+def iter_messages(client, get_username_from_id, get_channelname_from_id):
+  while True:
+    m = client.read()
+    yield m._replace(
+      thread=get_channelname_from_id(m.thread),
+      speaker=get_username_from_id(m.speaker),
+    )
+
+def send_messages(messages, client, get_channelid_from_name):
+  for m in messages:
+    client.send(m._replace(thread=get_channelid_from_name(m.thread)))
--- fb/client.js
+++ fb/client.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 var login = require("facebook-chat-api");
-var fastCsv = require("fast-csv");
 var csv = require("csv");
 
 if (process.env.SALAZAR_FB_EMAIL === undefined) throw new Error('SALAZAR_FB_EMAIL should be set');
 if (process.env.SALAZAR_FB_PASSWORD === undefined) throw new Error('SALAZAR_FB_PASSWORD should be set');
 
-// var csvWriter = csv.createWriteStream()
 var csvWriter = csv.stringify()
 csvWriter.pipe(process.stdout)
 
@@ -21,4 +19,9 @@ login({email: process.env.SALAZAR_FB_EMAIL, password: process.env.SALAZAR_FB_PAS
           message.body]);
       }
     });
+
+    var sendingStream = csv.parse().on('data', function(data) {
+      api.sendMessage(data[3], data[1])
+    });
+    process.stdin.pipe(sendingStream);
 });
